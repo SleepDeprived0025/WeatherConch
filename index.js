706df09630fb4ab63cf3a9b5aef38892c93cca55
SleepDeprived0025 <sleeplessnight1909@gmail.com>
2024-10-08 13:15:08 +0200

    finished functionality

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
 	res.render("index.ejs", {
-        apiKey: null});
+		apiKey: null,
+	});
 });
 
 app.post("/", (req, res) => {
@@ -20,27 +21,38 @@ app.post("/", (req, res) => {
 	let cityLan;
 	let cityLon;
 	try {
-		const locationResponse = axios.get(
-			`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
-        ).then((response) => {
-            console.log(response.data[0].lat, response.data[0].lon);
-            cityLan = response.data[0].lat;
-            cityLon = response.data[0].lon;
-            return axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?lat=${cityLan}&lon=${cityLon}&units=${units}&appid=${apiKey}`
-            ).then((response) => {
-                res.render("index.ejs", {
-                    city: response.data.name,
-                    temp: Math.round(response.data.main.temp),
-                    realTemp: Math.round(response.data.main.feels_like),
-                    desc: response.data.weather[0].description,
-                    metricUnit: units
-                })
-            });
-        });
+		const locationResponse = axios
+			.get(
+				`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
+			)
+			.then((response) => {
+				console.log(response.data[0].lat, response.data[0].lon);
+				cityLan = response.data[0].lat;
+				cityLon = response.data[0].lon;
+				return axios
+					.get(
+						`https://api.openweathermap.org/data/2.5/weather?lat=${cityLan}&lon=${cityLon}&units=${units}&appid=${apiKey}`
+					)
+					.then((response) => {
+						res.render("index.ejs", {
+							city: response.data.name,
+							temp: Math.round(response.data.main.temp),
+							realTemp: Math.round(response.data.main.feels_like),
+							desc: response.data.weather[0].description,
+                            icon: "http://openweathermap.org/img/wn/" + response.data.weather[0].icon + "@2x.png",
+							metricUnit: units,
+						});
+					});
+			});
 	} catch (error) {
-		throw new error;
+		throw new error();
 	}
+});
+
+app.post("/reset", (req, res) => {
+    res.render("index.ejs", {
+        apiKey: null,
+    });
 });
 
 app.listen(port, () => {
